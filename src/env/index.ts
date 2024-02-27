caaaf32e7d74016b301bf0d4b30e57a55a5893ac
Erik <ol.erik0107@gmail.com>
2024-02-27 18:56:53 -0300

    feat: creating .env wrapper

---
+++ src/env/index.ts
@@ -0,0 +1,29 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'prod', 'test']).default('test'),
+  PORT: z.coerce.number().default(3333),
+  DB_TYPE: z.enum(['pg', 'mysql', 'sqlite']).default('pg'),
+  DB_URL: z.string(),
+  DB_NAME: z.string(),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
