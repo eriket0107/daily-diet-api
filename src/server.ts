a6f61cdb5de6988d33d97a2eacfcb7e7d4681475
Erik <ol.erik0107@gmail.com>
2024-02-28 06:53:12 -0300

    feat: server response

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import { app } from './app'
 import { env } from './env'
 const port = env.PORT || 3333
 
+app.get('/', (request, reply) => {
+  return reply.send('OK')
+})
+
 app
   .listen({
     port,
