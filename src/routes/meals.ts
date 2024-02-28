ebec046c0e9afab5fd1d08ecdd6584427a90a3cf
Erik <ol.erik0107@gmail.com>
2024-02-28 08:36:01 -0300

    feat: adding route to create meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,32 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+import { knex } from '../../db/config'
+import { checkSessionId } from '../middlewares/check-session-id'
 
 export const mealsRoutes = async (app: FastifyInstance) => {
-  app.post('/', async (request, reply) => {
-    return reply.send('Meals OK')
+  app.post('/', { preHandler: [checkSessionId] }, async (request, reply) => {
+    const createMealSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isOnDiet: z.boolean(),
+      date: z.coerce.date(),
+    })
+
+    const { name, date, description, isOnDiet } = createMealSchema.parse(
+      request.body,
+    )
+
+    const sessionUser = request.user
+
+    await knex('meals').insert({
+      name,
+      description,
+      date: date.toUTCString(),
+      is_on_diet: isOnDiet,
+      user_id: sessionUser?.id,
+    })
+
+    return reply.status(201).send()
   })
 }
