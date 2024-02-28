ea15c2feb18076281b7c85e2841a185e3ba46e3d
Erik <ol.erik0107@gmail.com>
2024-02-28 06:24:47 -0300

    chore: adapting db table names

--- db/migrations/20240227202625_meals.ts
+++ db/migrations/20240227202625_meals.ts
@@ -1,7 +1,7 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('meal', (table) => {
+  await knex.schema.createTable('meals', (table) => {
     table.uuid('id').defaultTo(knex.fn.uuid()).primary()
     table.string('name').notNullable()
     table.string('description').notNullable()
@@ -11,5 +11,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('meal')
+  await knex.schema.dropTable('meals')
 }
--- db/migrations/20240227203307_add-user-id-in-meal-table.ts
+++ db/migrations/20240227203307_add-user-id-in-meal-table.ts
@@ -12,7 +12,7 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('meal', (table) => {
+  await knex.schema.alterTable('meals', (table) => {
     table.dropColumn('user_id')
   })
 }
