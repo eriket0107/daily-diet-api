3d5ec4d79812426b7d1052016c58e908dd6cb281
Erik <ol.erik0107@gmail.com>
2024-02-28 06:54:38 -0300

    feat: creating user route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import cookie from '@fastify/cookie'
 import fastify, { FastifyInstance } from 'fastify'
 
+import { userRoutes } from './routes/users'
+
 export const app: FastifyInstance = fastify()
 
 app.register(cookie)
+app.register(userRoutes, { prefix: '/users' })
---
+++ src/routes/users.ts
@@ -0,0 +1,40 @@
+import { randomUUID } from 'node:crypto'
+
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+import { knex } from '../../db/config'
+
+export const userRoutes = async (app: FastifyInstance) => {
+  app.post('/', async (request, reply) => {
+    const createUserSchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    const { email, name } = createUserSchema.parse(request.body)
+
+    const emailAlreadyExists = await knex('users').where({ email }).first()
+
+    if (emailAlreadyExists) {
+      return reply.status(401).send({
+        message: 'Unauthorized. Email already exists',
+      })
+    }
+
+    await knex('users').insert({ name, email, session_id: sessionId })
+
+    return reply.status(201).send()
+  })
+}
