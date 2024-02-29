7a9fc38e4c55ee77c219cda9d7854445a6c16d92
Erik <ol.erik0107@gmail.com>
2024-02-28 21:38:47 -0300

    feat: creating route to delete one meal

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -29,6 +29,7 @@ export const mealsRoutes = async (app: FastifyInstance) => {
 
     return reply.status(201).send()
   })
+
   app.put(
     '/:mealId',
     { preHandler: [checkSessionId] },
@@ -45,9 +46,12 @@ export const mealsRoutes = async (app: FastifyInstance) => {
 
       if (!mealId) return reply.status(404).send('Unable to use meald ID')
 
-      const meal = await knex('meals').where({ id: mealId }).first()
+      const mealToUpdate = await knex('meals')
+        .select()
+        .where({ id: mealId })
+        .first()
 
-      if (!meal) return reply.status(404).send('Meal not found')
+      if (!mealToUpdate) return reply.status(404).send('Meal not found')
 
       const { name, description, date, isOnDiet } = updateMealBodySchema.parse(
         request.body,
@@ -56,12 +60,37 @@ export const mealsRoutes = async (app: FastifyInstance) => {
       await knex('meals')
         .where({ id: mealId })
         .update({
-          name: name || meal?.name,
-          description: description || meal?.description,
-          is_on_diet: isOnDiet || meal?.is_on_diet,
-          date: date?.toUTCString() || meal?.date,
+          name: name || mealToUpdate?.name,
+          description: description || mealToUpdate?.description,
+          is_on_diet: isOnDiet || mealToUpdate?.is_on_diet,
+          date: date?.toUTCString() || mealToUpdate?.date,
           updated_at: new Date().toUTCString(),
         })
+
+      return reply.status(204).send()
+    },
+  )
+
+  app.delete(
+    '/:mealId',
+    { preHandler: [checkSessionId] },
+    async (request, reply) => {
+      const paramsSchema = z.object({
+        mealId: z.string(),
+      })
+
+      const { mealId } = paramsSchema.parse(request.params)
+
+      if (!mealId) return reply.status(404).send('Unable to use meald ID')
+
+      const mealToDelete = await knex('meals')
+        .select()
+        .where({ id: mealId })
+        .first()
+
+      if (!mealToDelete) return reply.status(404).send('Meal not found')
+
+      await knex('meals').where({ id: mealId }).delete()
 
       return reply.status(204).send()
     },
