e1b4410f258c34fe85cdcfffc91cc4e0c81b99e6
Erik <ol.erik0107@gmail.com>
2024-02-28 21:08:15 -0300

    feat: creating update meal route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -29,4 +29,41 @@ export const mealsRoutes = async (app: FastifyInstance) => {
 
     return reply.status(201).send()
   })
+  app.put(
+    '/:mealId',
+    { preHandler: [checkSessionId] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string() })
+      const updateMealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        isOnDiet: z.boolean().optional(),
+        date: z.coerce.date().optional(),
+      })
+
+      const { mealId } = paramsSchema.parse(request.params)
+
+      if (!mealId) return reply.status(404).send('Unable to use meald ID')
+
+      const meal = await knex('meals').where({ id: mealId }).first()
+
+      if (!meal) return reply.status(404).send('Meal not found')
+
+      const { name, description, date, isOnDiet } = updateMealBodySchema.parse(
+        request.body,
+      )
+
+      await knex('meals')
+        .where({ id: mealId })
+        .update({
+          name: name || meal?.name,
+          description: description || meal?.description,
+          is_on_diet: isOnDiet || meal?.is_on_diet,
+          date: date?.toUTCString() || meal?.date,
+          updated_at: new Date().toUTCString(),
+        })
+
+      return reply.status(204).send()
+    },
+  )
 }
